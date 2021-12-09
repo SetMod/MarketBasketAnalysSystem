4c76c930b25a91b93c4e9e14dbf1f5bdccfb2c06
SetMod <voroncka.andrey@gmail.com>
2021-12-09 12:00:45 +0200

    feat: added store and sevices

--- market-basket-analys-system-app/src/store/index.js
+++ market-basket-analys-system-app/src/store/index.js
@@ -1,11 +1,29 @@
 import { createStore } from 'vuex'
+import TransactionsService from '../services/TransactionsService'
+// import axios from 'axios';
 
 export default createStore({
   state: {
+    transactions: [],
+    isLoaded: false,
+    errorMsg: "",
+    backend_url: "http://127.0.0.1:3000",
   },
   mutations: {
+    setTransactions(state, payload) {
+      state.transactions = payload.data
+    },
+    setLoaded(state, payload) {
+      state.isLoaded = payload.data
+    },
+    setError(state, payload) {
+      state.errorMsg = payload.data
+    },
   },
   actions: {
+    getTransactions(context, payload) {
+      TransactionsService.getTransactions(context,payload)
+    },
   },
   modules: {
   }
