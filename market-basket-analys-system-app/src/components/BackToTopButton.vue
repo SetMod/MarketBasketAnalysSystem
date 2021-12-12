82be669bf92f8a41f1a4ba3d329c0b696d66d03c
SetMod <voroncka.andrey@gmail.com>
2021-12-12 18:45:14 +0200

    feat: added new component

---
+++ market-basket-analys-system-app/src/components/BackToTopButton.vue
@@ -0,0 +1,51 @@
+<template>
+  <button
+    type="button"
+    class="btn btn-dark btn-floating position-fixed btn-back-to-top"
+    @click="backToTop"
+    v-show="display"
+  >
+    Top
+  </button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      display: true,
+      distance: 250,
+    };
+  },
+  methods: {
+    scrollFunction() {
+      if (
+        document.body.scrollTop > this.distance ||
+        document.documentElement.scrollTop > this.distance
+      ) {
+        this.display = true;
+      } else {
+        this.display = false;
+      }
+    },
+    backToTop() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    },
+  },
+  mounted() {
+    this.scrollFunction();
+    window.addEventListener("scroll", this.scrollFunction);
+  },
+  unmounted() {
+    window.removeEventListener("scroll", this.scrollFunction);
+  },
+};
+</script>
+
+<style>
+.btn-back-to-top {
+  bottom: 20px;
+  right: 20px;
+}
+</style>
