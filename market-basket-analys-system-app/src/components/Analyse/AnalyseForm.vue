ebe91044046559a9e671fc4674308e754d642706
SetMod <voroncka.andrey@gmail.com>
2021-12-15 23:33:46 +0200

    feat: added analyseform

--- market-basket-analys-system-app/src/components/Analyse/AnalyseForm.vue
+++ market-basket-analys-system-app/src/components/Analyse/AnalyseForm.vue
@@ -1,9 +1,17 @@
 <template>
   <form class="row g-3">
     <div class="col">
-      <AnalyseInput inputName="Support" @input="inputSupport" />
-      <AnalyseInput inputName="Confidence" v-model="confidence" />
-      <AnalyseInput inputName="Lift" v-model="lift" />
+      <AnalyseInput
+        inputName="Support"
+        @change="onSupportChange"
+        :value="support"
+      />
+      <AnalyseInput
+        inputName="Confidence"
+        @change="onConfidenceChange"
+        :value="confidence"
+      />
+      <AnalyseInput inputName="Lift" @change="onLiftChange" :value="lift" />
     </div>
 
     <div class="mb-3 form-check">
@@ -11,41 +19,68 @@
       <label class="form-check-label" for="exampleCheck1">Check me out</label>
     </div>
 
-    <button type="submit" class="btn btn-primary" @click="onSupportInput">
+    <button type="submit" class="btn btn-primary" @click.prevent="onSubmit">
       Submit
     </button>
   </form>
 </template>
 
 <script>
+import store from "@/store/index.js";
 import AnalyseInput from "./AnalyseInput.vue";
+import { mapActions, mapMutations, mapState } from "vuex";
 export default {
   name: "AnalyseForm",
-  data() {
-    return {
-      support: 0.01,
-      confidence: 0.01,
-      lift: 0.01,
-    };
-  },
-  methods: {
-    inputSupport(e) {
-      try {
-        this.support = parseFloat(e.target.value);
-      } catch (error) {
-        console.error(error);
-        this.support = 0.01;
-      }
-    },
-    onSupportInput(e) {
-      e.preventDefault();
-      console.log(this.support);
-      console.log(this.confidence);
-      console.log(this.lift);
-    },
-  },
   components: {
     AnalyseInput,
+  },
+  store,
+  computed: {
+    ...mapState({
+      support: (state) => state.analyse.support,
+      confidence: (state) => state.analyse.confidence,
+      lift: (state) => state.analyse.lift,
+    }),
+  },
+  methods: {
+    ...mapMutations({
+      setSupport: "analyse/setSupport",
+      setConfidence: "analyse/setConfidence",
+      setLift: "analyse/setLift",
+    }),
+    ...mapActions({
+      getRules: "analyse/getRules",
+    }),
+    onSupportChange(e) {
+      try {
+        this.setSupport(parseFloat(e.target.value));
+      } catch (error) {
+        console.error(error);
+        this.setSupport(0.01);
+      }
+    },
+    onConfidenceChange(e) {
+      try {
+        this.setConfidence(parseFloat(e.target.value));
+      } catch (error) {
+        console.error(error);
+        this.setConfidence(0.1);
+      }
+    },
+    onLiftChange(e) {
+      try {
+        this.setLift(parseFloat(e.target.value));
+      } catch (error) {
+        console.error(error);
+        this.setLift(2);
+      }
+    },
+    onSubmit() {
+      this.getRules();
+      this.setSupport();
+      this.setConfidence();
+      this.setLift();
+    },
   },
 };
 </script>
