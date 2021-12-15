7896f2fb3d19485a688bb06c227b88be4d159ccf
SetMod <voroncka.andrey@gmail.com>
2021-12-15 23:31:12 +0200

    feat: added loading circel

---
+++ market-basket-analys-system-app/src/components/LoadingCircle.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="loading my-3">
+    <div class="spinner-grow" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoadingCircle",
+};
+</script>
+
+<style>
+</style>
